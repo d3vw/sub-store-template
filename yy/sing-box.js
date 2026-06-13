8f03f2cf5018a1ce38ec792fc254417a1ea5dc13
Grey Moore <contact@grey.ac>
2026-06-13 20:15:13 +0800

    typo

--- yy/sing-box.js
+++ yy/sing-box.js
@@ -17,7 +17,7 @@ config.outbounds.push(...proxies)
 
 config.outbounds.map(i => {
   // Populate feature selectors with proxies
-  if (['proxy', 'google', 'claude', 'polomarket', 'youtube', 'ai'].includes(i.tag)) {
+  if (['proxy', 'google', 'claude', 'polymarket', 'youtube', 'ai'].includes(i.tag)) {
     i.outbounds.push(...getTags(proxies, /feat/i))
   }
   if (['all', 'all-auto'].includes(i.tag)) {
